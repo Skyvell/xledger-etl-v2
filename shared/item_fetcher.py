6b8b2cda6671a2886a5497cf769b1af81c7ff561
Ted Skyvell <tskyvell@gmail.com>
2024-06-12 13:24:44 +0200

    Experiment with results class.

--- shared/item_fetcher.py
+++ shared/item_fetcher.py
@@ -3,18 +3,41 @@ from typing import Dict, Any, Set, List, Optional
 import logging
 
 
+class ItemsResult:
+    def __init__(self, items: list[Dict], last_deltas_cursor: str = None) -> None:
+        self.items = items
+        self.last_deltas_cursor = last_deltas_cursor
+
+    def has_items(self) -> bool:
+        return bool(self.items)
+    
+    def get_items(self) -> List[Dict]:
+        return self.items
+    
+    def get_last_deltas_cursor(self) -> str:
+        return self.last_deltas_cursor
+    
+    def get_last_item_cursor(self) -> str:
+        return self.items[-1]['cursor'] if self.items else None
+    
+    def add_key_to_all_items(self, key: str, value: Any) -> None:
+        for item in self.items:
+            item[key] = value
+
+
 class ItemFetcher:
     def __init__(self, client: GraphQLClient, query_by_dbids: str, query_by_cursor: str) -> None:
         self.graphql_client = client
         self.query_by_dbids = query_by_dbids
         self.query_by_cursor = query_by_cursor
 
-    def fetch_items_by_ids(self, db_ids: List[str], first: int = 10000, add_values) -> PaginationQueryResult:
+    def fetch_items_by_ids(self, db_ids: List[str], first: int = 10000) -> PaginationQueryResult:
         if not db_ids:
-            return []
+            return ItemsResult([])
         
         variables = {"first": first, "dbIdList": db_ids}
-        return self._execute_paginated_query(self.query_by_dbids, variables)
+        query_resut = self._execute_paginated_query(self.query_by_dbids, variables)
+        return ItemsResult(query_resut.get_nodes(), query_resut.get_last_cursor())
 
     def fetch_all_items_after_cursor(self, after: Optional[str], first: int = 10000) -> PaginationQueryResult:
         variables = {"first": first, "after": after}
